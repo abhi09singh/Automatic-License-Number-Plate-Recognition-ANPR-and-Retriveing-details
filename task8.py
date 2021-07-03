7f49542a78f74778ebc715ac5b34720eaaa722c8
Abhinav Singh <62004142+abhi09singh@users.noreply.github.com>
2021-07-03 17:05:39 +0530

    Add files via upload

---
+++ task8.py
@@ -0,0 +1,65 @@
+#! /usr/bin/python3
+
+print("content-type: text/html")
+print()
+
+
+print('''<style>
+pre{
+color: black;
+font-weight: bold;
+font-size: 20px;
+}
+</style>''')
+
+
+import cgi
+import subprocess as sb
+
+fs = cgi.FieldStorage()
+
+plate_no = fs.getvalue("x")
+
+
+if plate_no == "MH 20 EE 7602":
+    print("<body style='padding: 40px;'>")
+    print('<h1 style="color:#df405a;" >Output</h1>')
+    print('''<pre>
+    Registration Name : ABHINAV SINGH
+    License No : 12345678910
+    Make / Model : Skoda / OCtavia
+    Registration Date : 1/1/2017
+    Registering Authority : UTTAR PRADESH , INDIA
+    Vehicle Class : MCWG
+    Fuel Type : CNG
+    Engine No : IVK3N1734538
+    Chassis No : HMSURVWVQSVWE
+    Insurance Upto : 5/13/2022
+    Fitness Upto : 4/8/2030
+    </pre>''')
+    print("</body>")
+
+elif plate_no == "MH 20 EJ 0364":
+    print("<body style='padding: 40px;'>")
+    print('<h1 style="color:#df405a;" >Output</h1>')
+    print('''<pre>
+    Registration Name : KAUSTUBH RAGHUVANSHI
+    License No : 10987654321
+    Make / Model : Skoda / Kodiaq
+    Registration Date : 1/1/2019
+    Registering Authority : UTTAR PRADESH, INDIA
+    Vehicle Class : MCWG
+    Fuel Type : CNG
+    Engine No : IVK3N1734538
+    Chassis No : HMSURVWVQSVWE
+    Insurance Upto : 5/13/2022
+    Fitness Upto : 4/8/2030
+    </pre>''')
+    print("</body>")
+
+
+else:
+    print("<body style='padding: 40px;'>")
+    print('<h1 style="color:#df405a;" >Output</h1>')
+    print("No data available for this number...")
+    print("</body>")
